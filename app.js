5802345c443510b197e590cac9ff452794f1c97a
zjacky0409 <0409jacky0409@gmail.com>
2021-11-25 23:01:18 +0800

    Build a record router
    Just a simple version and it do not connect to the db.
    
    And we need to build a db to save the reocrd

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var exercisesRouter = require('./routes/exercises');
+var records = require('./routes/record');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/exercises', exercisesRouter);
+app.use('/records', records);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+let exercises =  [{"exercises":"Lat Pull Down"},{"exercises":"RDL"},{"exercises":"Squat"},{"exercises":"Pull Up"},{"exercises":"Bench Incline"}]
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
